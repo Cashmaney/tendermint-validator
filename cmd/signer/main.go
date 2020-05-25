686ed534dc82cc9492d5848f1a2b9b17a454b370
Itzik Grossman <itzik@keytango.io>
2020-05-25 18:19:12 +0300

    Parsing Cosmos validator key format

--- cmd/signer/main.go
+++ cmd/signer/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"flag"
 	"fmt"
-	"github.com/tendermint/tendermint/crypto/ed25519"
 	tmlog "github.com/tendermint/tendermint/libs/log"
 	tos "github.com/tendermint/tendermint/libs/os"
 	svc "github.com/tendermint/tendermint/libs/service"
-	"golang.org/x/crypto/ssh/terminal"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -20,52 +16,12 @@ import (
 	"time"
 )
 
-const (
-	PrivateKeyLength = 64
-)
-
 func fileExists(filename string) bool {
 	info, err := os.Stat(filename)
 	if os.IsNotExist(err) {
 		return false
 	}
 	return !info.IsDir()
-}
-
-func openPrivateKeyFile(filename string) (ed25519.PrivKeyEd25519, error) {
-	cert, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return ed25519.PrivKeyEd25519{}, err
-	}
-	text := string(cert)
-
-	key, err := base64.StdEncoding.DecodeString(text)
-	if err != nil {
-		return ed25519.PrivKeyEd25519{}, err
-	}
-
-	if len(key) != PrivateKeyLength {
-		panic(fmt.Sprintf("Tried to import key with an invalid key size %d, Expected 64", len(key)))
-	}
-
-	var privKey ed25519.PrivKeyEd25519
-	copy(privKey[:], key)
-
-	return privKey, nil
-}
-
-func savePrivateKeyFile(filename string, privKey ed25519.PrivKeyEd25519) error {
-
-	fmt.Println(len(privKey[:]))
-
-	byteB64 := base64.StdEncoding.EncodeToString(privKey[:])
-
-	err := ioutil.WriteFile(filename, []byte(byteB64), 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func main() {
@@ -98,55 +54,12 @@ func main() {
 	flag.Parse()
 
 	if *keyImport != "" {
-		fmt.Print("Enter Password: ")
-		bytePassword, err := terminal.ReadPassword(0)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Print("\nRepeat Password: ")
-		bytePassword2, err := terminal.ReadPassword(0)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Print("\n")
-		if string(bytePassword) != string(bytePassword2) {
-			log.Fatal("Passwords do not match")
-		}
-		if len(bytePassword2) == 0 {
-			log.Fatal("Password cannot be empty")
-		}
-
-		privKey, err := openPrivateKeyFile(*keyImport)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = val.ImportKey(privKey, bytePassword)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(fmt.Sprintf("Imported key successfully!"))
+		signer.ImportKey(*val, *keyImport)
 		return
 	}
 
 	if *keyExport != "" {
-
-		fmt.Print("Enter Password: ")
-		bytePassword, err := terminal.ReadPassword(0)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		res, err := val.ExportKey(bytePassword)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = savePrivateKeyFile(*keyExport, res)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(fmt.Sprintf("Exported key successfully!"))
+		signer.ExportKey(*val, *keyExport)
 		return
 	}
 
---
+++ internal/signer/KeyUtils.go
@@ -0,0 +1,157 @@
+package signer
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"golang.org/x/crypto/ssh/terminal"
+	"io/ioutil"
+	"log"
+	teeval "tendermint-signer/tee_validator"
+)
+
+const (
+	PrivateKeyLength = 64
+)
+
+func ImportKey(val teeval.EnclavePV, importPath string) {
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("\nRepeat Password: ")
+	bytePassword2, err := terminal.ReadPassword(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("\n")
+	if string(bytePassword) != string(bytePassword2) {
+		log.Fatal("Passwords do not match")
+	}
+	if len(bytePassword2) == 0 {
+		log.Fatal("Password cannot be empty")
+	}
+
+	privKey, err := openPrivateKeyFile(importPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = val.ImportKey(privKey, bytePassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(fmt.Sprintf("Imported key successfully!"))
+	return
+}
+
+func ExportKey(val teeval.EnclavePV, exportPath string) {
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := val.ExportKey(bytePassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = savePrivateKeyFile(exportPath, res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Exported key successfully!"))
+	return
+}
+
+func savePrivateKeyFile(filename string, privKey ed25519.PrivKeyEd25519) error {
+
+	fmt.Println(len(privKey[:]))
+
+	var keyFile CosmosValidatorKey
+
+	var pubkeyBytes [32]byte
+
+	copy(pubkeyBytes[:], privKey[32:])
+
+	pubKey := base64.StdEncoding.EncodeToString(pubkeyBytes[:])
+	byteB64 := base64.StdEncoding.EncodeToString(privKey[:])
+	address := privKey.PubKey().Address().String()
+
+	keyFile.PrivKey = TypeVal{
+		Type:  "tendermint/PrivKeyEd25519",
+		Value: byteB64,
+	}
+	keyFile.PubKey = TypeVal{
+		Type:  "tendermint/PrivKeyEd25519",
+		Value: pubKey,
+	}
+	keyFile.Address = address
+
+	jsonBytes, err := json.Marshal(keyFile)
+
+	err = ioutil.WriteFile(filename, jsonBytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func openPrivateKeyFile(filename string) (ed25519.PrivKeyEd25519, error) {
+	keyFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return ed25519.PrivKeyEd25519{}, err
+	}
+
+	// Try to parse a cosmos private validator key
+	privKeyFile := parseCosmosKeyFile(keyFile)
+	if privKeyFile == "" {
+		// if that fails, just try the base64 string
+		privKeyFile = string(keyFile)
+	}
+
+	key, err := base64.StdEncoding.DecodeString(privKeyFile)
+	if err != nil {
+		return ed25519.PrivKeyEd25519{}, err
+	}
+
+	if len(key) != PrivateKeyLength {
+		panic(fmt.Sprintf("Tried to import key with an invalid key size %d, Expected 64", len(key)))
+	}
+
+	var privKey ed25519.PrivKeyEd25519
+	copy(privKey[:], key)
+
+	return privKey, nil
+}
+
+type TypeVal struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+// User struct which contains a name
+// a type and a list of social links
+type CosmosValidatorKey struct {
+	Address string  `json:"address"`
+	PubKey  TypeVal `json:"pub_key"`
+	PrivKey TypeVal `json:"priv_key"`
+}
+
+func parseCosmosKeyFile(file []byte) string {
+	var keyFile CosmosValidatorKey
+	err := json.Unmarshal(file, &keyFile)
+	if err != nil {
+		return ""
+	}
+
+	if keyFile.PrivKey.Type != "tendermint/PrivKeyEd25519" {
+		log.Fatalf("Unsupported key type %s - must be Ed25519", keyFile.PrivKey.Type)
+	}
+
+	return keyFile.PrivKey.Value
+}
